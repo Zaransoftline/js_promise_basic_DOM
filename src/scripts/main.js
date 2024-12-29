ece5cc52078be19e55ba6c9b8c698753fca10f46
Zaransoftline <131307196+Zaransoftline@users.noreply.github.com>
2024-12-29 17:03:49 -0500

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
 
 // write your code here
+function success() {
+  const message = document.createElement('div');
+  message.textContent = 'Promise was resolved!';
+  document.querySelector('body').appendChild(message);
+}
+
+function error() {
+  const message = document.createElement('div');
+  message.textContent = 'Promise was rejected!';
+  document.querySelector('body').appendChild(message);
+}
+
+const promise1 = new Promise((resolve, reject) => {
+  const button = document.querySelector('h1');
+
+  button.addEventListener('click', () => {
+    if (button) {
+      resolve('success');
+    } else {
+      reject(new Error('Promise rejected'));
+    }
+  });
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error('Promise rejected'));
+    error();
+  }, 3000);
+});
+
+Promise.race([promise1, promise2])
+  .then(() => {
+    success();
+  })
+  .catch(() => {
+    error();
+  });
